536769de983f3c0536018082cd7018c0927d368d
Zeynepnur Cavcar <zeynepnur.cavcar@ug.bilkent.edu.tr>
2021-10-20 19:50:21 +0300

    fix crashing problem

--- src/MaterialManager.py
+++ src/MaterialManager.py
@@ -25,4 +25,11 @@ class MaterialManager:
         return self._announceMaterial(returnText)
 
     def _announceMaterial(self, text: str) -> bool:
-        return self.announcer(text)
+        returnValue = False
+         try:
+             returnValue = self.announcer(text)
+         except:
+            print("Announcement of material type is failed.")
+            returnValue = False
+        finally:
+            return returnValue
